31c923242996fb1d17d68521b9246fed02c568a7
Mark Mavis <mark.mavis@outlook.com>
2025-03-10 01:10:40 -0700

    Class update

--- models/recurring_transaction.py
+++ models/recurring_transaction.py
@@ -1,18 +1,29 @@
-import models.transaction as transaction
+from models.transaction import Transaction
 from datetime import date
 
-class RecurringTransaction(transaction):
-    def __init__(self, ammount: str, type: str, category: str, payee: str, issuer: str, start_date: date, end_date: date, recurrence_period: str, next_due_date: date, is_active: bool = True):
+class RecurringTransaction(Transaction):
+    def __init__(self, 
+                 ammount: float, 
+                 type: str, 
+                 category: str, 
+                 payee: str, 
+                 issuer: str, 
+                 start_date: date, 
+                 end_date: date, 
+                 recurrence_period: str, 
+                 next_due_date: date, 
+                 active: bool = True):
         super().__init__(ammount, type, category, payee, issuer)
         self.start_date = start_date
         self.end_date = end_date
         self.recurrence_period = recurrence_period
         self.next_due_date = next_due_date
+        self.active = active
        
     def is_active(self) -> bool:
-        return self.is_active
+        return self.active
     def calculate_next_due_date(self) -> date:
         pass
-    def generate_next_transaction(self) -> transaction:
-        pass
+    def generate_transaction(self) -> Transaction:
+        return Transaction(self.amount, self.type, self.category, self.payee, self.issuer)
     
--- services/transaction_manager.py
+++ services/transaction_manager.py
@@ -1,28 +1,45 @@
-import models.transaction as Transaction
-import datetime as dt
+from models.transaction import Transaction
+from models.recurring_transaction import RecurringTransaction
+import schedule
+
+from datetime import datetime
 
 class TransactionManager:
     def __init__(self):
         self.transactions = []
         self.recurring_transactions = []
+        self.load_transactions()
         
-    def add_transaction(self):
-        pass
+    def add_transaction(self, transaction: Transaction):
+        self.transactions.append(transaction)
+        self.save_transactions()
     
-    def add_recurring_transaction(self):
-        pass
+    def add_recurring_transaction(self, transaction: RecurringTransaction):
+        self.recurring_transactions.append(transaction)
+        self.save_transactions()
     
-    def delete_transaction(self):
-        pass
+    def delete_transaction(self, transaction_id: str):
+        self.transactions =[t for t in self.transactions if t.id != transaction_id]
+        self.save_transactions()
+        
+    def get_transactions_by_category(self, category: str):
+        return [t for t in self.transactions if t.category.lower() == category.lower()]
     
     def process_recurring_transactions(self):
         for transaction in self.recurring_transactions:
-            if transaction.is_active() and transaction.calculate_next_due_date() <= dt.datetime.now():
-                next_transaction = transaction.generate_next_transaction()
+            if transaction.is_active() and transaction.calculate_next_due_date() <= datetime.now():
+                next_transaction = transaction.generate_transaction()
                 self.add_transaction(next_transaction)
     
-    def get_transactions_by_category(self):
+    def load_transactions(self):
         pass
     
     def generate_report(self):
         pass
+    
+    def save_transactions(self):
+        """Saves transactions to a JSON file."""
+        data = {
+            "transactions": [vars(t) for t in self.transactions],
+            "recurring_transactions": [vars(t) for t in self.recurring_transactions]
+        }
