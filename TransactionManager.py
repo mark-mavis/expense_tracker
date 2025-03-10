8f4090a55562e9e81deafeb078f40e5ba336b4ce
Mark Mavis <mark.mavis@outlook.com>
2025-03-09 20:33:36 -0700

    add TransactionManager class

---
+++ TransactionManager.py
@@ -0,0 +1,24 @@
+import Transaction
+
+class TransactionManager:
+    def __init__(self):
+        self.transactions = []
+        self.recurring_transactions = []
+        
+    def add_transaction(self):
+        pass
+    
+    def add_recurring_transaction(self):
+        pass
+    
+    def delete_transaction(self):
+        pass
+    
+    def process_recurring_transactions(self):
+        pass
+    
+    def get_transactions_by_category(self):
+        pass
+    
+    def generate_report(self):
+        pass
